2203f23cc4da5f0cb3e2d3998cdd70aba3291e07
Abhishek Kumar <abhishekbusiness25802@gmail.com>
2025-08-18 12:49:41 -0700

    Add new changes

--- sonar_fix_agent/config.py
+++ sonar_fix_agent/config.py
@@ -9,3 +9,10 @@ SONAR_TOKEN = os.getenv("SONAR_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 SONAR_URL = os.getenv("SONAR_URL", "https://sonarcloud.io")
 MAX_FIX_LINES = int(os.getenv("MAX_FIX_LINES", "10"))
+
+# Debug prints to verify environment variables
+print("GITHUB_TOKEN loaded:", bool(GITHUB_TOKEN))
+print("SONAR_TOKEN loaded:", bool(SONAR_TOKEN))
+print("OPENAI_API_KEY loaded:", bool(OPENAI_API_KEY))
+print("SONAR_URL loaded:", SONAR_URL)
+print("MAX_FIX_LINES:", MAX_FIX_LINES)
